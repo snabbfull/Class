db94d1df5c51fdc76bbbfaf3210a2b06f5b5c11c
Игорь Дмитриев <igor_dmitriev@MacBook-Air-de-Igor.local>
2025-08-13 19:27:30 +0300

    Corrections

--- src/index.js
+++ src/index.js
@@ -1,46 +1,68 @@
 export class Character {
-    constructor(name, type, health, level, attack, defence) {
-        if (typeof name !== 'string') {
-            throw new Error('Имя должно быть строкой');
-        }
-        if (name.length < 2 || name.length > 10) {
-            throw new Error('Длина имени должна быть от 2 до 10 символов');
-        }
-        
+    constructor(name, type) {
         const allowedTypes = ['Bowman', 'Swordsman', 'Magician', 'Daemon', 'Undead', 'Zombie'];
 
-        if (typeof type !== 'string' || !allowedTypes.includes(type)) {
+        if (typeof name !== 'string' || name.length < 2 || name.length > 10) {
+            throw new Error('Имя должно быть строкой длиной от 2 до 10 символов');
+        }
+
+        if (!allowedTypes.includes(type)) {
             throw new Error(`Тип должен быть одним из: ${allowedTypes.join(', ')}`);
         }
+
         this.name = name;
         this.type = type;
-        this.health = health;
-        this.level = level;
-        this.attack = attack;
-        this.defence = defence;
+        this.health = 100;
+        this.level = 1;
+        this.attack = 0;
+        this.defence = 0;
     }
 }
 
 export class Bowerman extends Character {
-
+    constructor(name) {
+        super(name, 'Bowman');
+        this.attack = 25;
+        this.defence = 25;
+    }
 }
 
 export class Swordsman extends Character {
-
+    constructor(name) {
+        super(name, 'Swordsman');
+        this.attack = 40;
+        this.defence = 10;
+    }
 }
 
 export class Magician extends Character {
-
+    constructor(name) {
+        super(name, 'Magician');
+        this.attack = 10;
+        this.defence = 40;
+    }
 }
 
 export class Daemon extends Character {
-
+    constructor(name) {
+        super(name, 'Daemon');
+        this.attack = 10;
+        this.defence = 40;
+    }
 }
 
 export class Undead extends Character {
-
+    constructor(name) {
+        super(name, 'Undead');
+        this.attack = 25;
+        this.defence = 25;
+    }
 }
 
 export class Zombie extends Character {
-
+    constructor(name) {
+        super(name, 'Zombie');
+        this.attack = 40;
+        this.defence = 10;
+    }
 }
